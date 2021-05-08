e14e93af13f3ee2ca3f1ed546ff3cca5c6fd164d
#Verse <tanbinsiddiqui11@gmail.com>
2021-05-09 05:04:33 +0600

    09/05/2021

--- js/jquery.js
+++ js/jquery.js
@@ -5,5 +5,16 @@ $(document).ready(function(){
         $(".first-h1").slideToggle(500);
   
     });
+    $("#explore-btn").click(function(){
+  
+        $(".first-h3").slideToggle(500);
+  
+    });
+    $(".feature-text").hover(function(){
+
+        $(".light").addClass("animate__animated animate__flipInX");
+        $(".light").css("fontSize", "20px");
+
+    });
   
   });
